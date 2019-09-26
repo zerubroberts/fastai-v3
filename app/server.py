ae1ccb4d49b0d52232b28b0f3f65ca8c26e581ca
Zerub Roberts <zrmuthya@deakin.edu.au>
2019-09-26 15:26:37 +1000

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Bzl1pMelICWOWncZsPLlBhJzaDgYlJHs'
+export_file_url = 'https://www.dropbox.com/s/5xd6cpnw56nfkyj/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['construction_vest','construction_novest']
